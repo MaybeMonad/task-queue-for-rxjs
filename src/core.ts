031249e077d9b1e78208c573c35a50ab71b76cf7
Captain Martin <captainmartinisanerd@gmail.com>
2022-06-27 02:48:58 +0000

    chore: format

--- src/core.ts
+++ src/core.ts
@@ -8,155 +8,169 @@
  * [#status$::idle] -> [#taskPackages] -> [#queue$::throttleTime(500)] -> [Async Execution]
  */
 
- import { BehaviorSubject, concatMap, debounceTime, mergeMap, Observable, of, Subject, Subscription, zip } from 'rxjs'
+import {
+  BehaviorSubject,
+  concatMap,
+  debounceTime,
+  mergeMap,
+  Observable,
+  of,
+  Subject,
+  Subscription,
+  zip,
+} from "rxjs";
 
- interface Option {
-  taskPacakgeSize?: number
-  tickTime?: number
- }
+interface Option {
+  taskPacakgeSize?: number;
+  tickTime?: number;
+}
 
- interface Task extends Object {
-  __taskType__: 'async' | 'sync'
- }
+interface Task extends Object {
+  __taskType__: "async" | "sync";
+}
 
- export class TaskQueue<T> {
-   #tasksConsumer$: Subject<T | Task> = new Subject()
-   #queue$: Subject<Task[]> = new Subject()
-   /**
-    * Store the tasks in the latest queue.
-    */
-   #tasks: (Task | null)[] = []
-   #status$: BehaviorSubject<'idle' | 'running' | 'paused'> = new BehaviorSubject<'idle' | 'running' | 'paused'>('idle')
-   #taskPackages: Observable<Task>[] = []
-   #taskSourceSubscription: Subscription | null = null
-   #statusSubscription: Subscription | null = null
-   #queueSubscription: Subscription | null = null
-   #taskPacakgeSize = 1
-   #tickTime = 100
-   #allTasks = new WeakMap()
- 
-   constructor(subject: Subject<T>, opt?: Option) {
+export class TaskQueue<T> {
+  #tasksConsumer$: Subject<T | Task> = new Subject();
+  #queue$: Subject<Task[]> = new Subject();
+  /**
+   * Store the tasks in the latest queue.
+   */
+  #tasks: (Task | null)[] = [];
+  #status$: BehaviorSubject<"idle" | "running" | "paused"> =
+    new BehaviorSubject<"idle" | "running" | "paused">("idle");
+  #taskPackages: Observable<Task>[] = [];
+  #taskSourceSubscription: Subscription | null = null;
+  #statusSubscription: Subscription | null = null;
+  #queueSubscription: Subscription | null = null;
+  #taskPacakgeSize = 1;
+  #tickTime = 100;
+  #allTasks = new WeakMap();
+
+  constructor(subject: Subject<T>, opt?: Option) {
     // @ts-ignore
-     this.#tasksConsumer$ = subject
-     this.#taskPacakgeSize = opt?.taskPacakgeSize ?? this.#taskPacakgeSize
-     this.#tickTime = opt?.tickTime ?? this.#tickTime
-   }
- 
-   unsubscribe() {
-     this.#taskSourceSubscription?.unsubscribe()
-     this.#statusSubscription?.unsubscribe()
-     this.#queueSubscription?.unsubscribe()
-   }
- 
-   pause() {
-     this.#status$.next('paused')
-   }
+    this.#tasksConsumer$ = subject;
+    this.#taskPacakgeSize = opt?.taskPacakgeSize ?? this.#taskPacakgeSize;
+    this.#tickTime = opt?.tickTime ?? this.#tickTime;
+  }
 
-   next(x: T) {
-     const nextValue = new Object(x) as Task
-     nextValue.__taskType__ = 'sync'
-     this.#tasksConsumer$.next(nextValue)
+  unsubscribe() {
+    this.#taskSourceSubscription?.unsubscribe();
+    this.#statusSubscription?.unsubscribe();
+    this.#queueSubscription?.unsubscribe();
+  }
+
+  pause() {
+    this.#status$.next("paused");
+  }
+
+  next(x: T) {
+    const nextValue = new Object(x) as Task;
+    nextValue.__taskType__ = "sync";
+    this.#tasksConsumer$.next(nextValue);
+  }
+
+  nextAsync(x: T) {
+    const nextValue = new Object(x) as Task;
+    nextValue.__taskType__ = "async";
+    this.#tasksConsumer$.next(nextValue);
+  }
+
+  static async *asyncLoopGenerator<T>(x: Task[], exec: (x: T) => Promise<T>) {
+    let i = 0;
+
+    while (i < x.length) {
+      console.warn(`[Task Queue]: AsyncLoop::[running]::${x[i]}`);
+      if (x[i].__taskType__ === "async") {
+        await exec(x[i].valueOf() as T);
+      } else {
+        exec(x[i].valueOf() as T);
+      }
+      yield i++;
     }
-    
-    nextAsync(x: T) {
-     const nextValue = new Object(x) as Task
-     nextValue.__taskType__ = 'async'
-    this.#tasksConsumer$.next(nextValue)
-   }
- 
-   static async *asyncLoopGenerator<T>(x: Task[], exec: (x: T) => Promise<T>) {
-     let i = 0
- 
-     while (i < x.length) {
-       console.warn(`[Task Queue]: AsyncLoop::[running]::${(x[i])}`)
-       if (x[i].__taskType__ === 'async') {
-         
-         await exec(x[i].valueOf() as T)
-       } else {
-        exec(x[i].valueOf() as T)
-       }
-       yield i++
-     }
-   }
- 
-   subscribe(execution: (x: T) => Promise<T>) {
-     this.#queueSubscription = this.#queue$.pipe(debounceTime(this.#tickTime)).subscribe(async x => {
-       console.groupCollapsed(
-         `[Custom Events]: %c TaskQueue::taskPacakgeSize(${this.#taskPacakgeSize}) `,
-         `background: #1BA353; color: white; border-radius: 2px;`
-       )
- 
-       if (this.#status$.value === 'paused') return
- 
-       if (x instanceof Array) {
-         this.#status$.next('running')
-         for await (const result of TaskQueue.asyncLoopGenerator(x, execution)) {
-           this.#tasks[result] = null
- 
-           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-           // @ts-ignore
-           if (this.#status$.value === 'pause') break
-         }
- 
-         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-         // @ts-ignore
-         if (this.#status$.value === 'pause') return
- 
-         this.#tasks = []
-         this.#status$.next('idle')
-       }
- 
-       console.groupEnd()
-     })
- 
-     this.#taskSourceSubscription = this.#tasksConsumer$?.subscribe(x => {
-        
-      
-       switch (this.#status$.value) {
-         case 'idle':
-           if (this.#tasks.length < this.#taskPacakgeSize + 1) {
-             this.#tasks.push(x as Task)
-             this.#queue$.next(this.#tasks as Task[])
-           } else {
-             this.#status$.next('running')
-             this.#taskPackages.push(of(x as Task))
-           }
- 
-           break
- 
-         case 'paused':
-         case 'running':
-         default:
-           this.#taskPackages.push(of(x as Task))
-           break
-       }
-     })
- 
-     this.#statusSubscription = this.#status$.subscribe(status => {
-       console.warn(`[Custom Events]: TaskQueue::${status}`)
-       if (status === 'idle' && this.#taskPackages.length > 0) {
-         zip(this.#taskPackages.splice(0, this.#taskPacakgeSize))
-           .pipe(
-             mergeMap(i => {
-               // Manipulate the tasks in TasksPackageQueue before sending them to the queue
-               // Example:
-               // 1. If the tasks have [priority] property, sort them by priority
-               // 2. If the tasks have [delay] property, delay them by delay
-               // 3. If the tasks have [interval] property, delay them by interval
-               // 4. If the tasks have [sequence] property, delay them by sequence
-               // 5. If the tasks have [parallel] property, delay them by parallel
-               return of(i)
-             }),
-             concatMap(x => {
-               this.#queue$.next(x as Task[])
-               return x
-             })
-           )
-           .subscribe()
-       }
-     })
- 
-     return this
-   }
- }
- 
+  }
+
+  subscribe(execution: (x: T) => Promise<T>) {
+    this.#queueSubscription = this.#queue$
+      .pipe(debounceTime(this.#tickTime))
+      .subscribe(async (x) => {
+        console.groupCollapsed(
+          `[Custom Events]: %c TaskQueue::taskPacakgeSize(${
+            this.#taskPacakgeSize
+          }) `,
+          `background: #1BA353; color: white; border-radius: 2px;`
+        );
+
+        if (this.#status$.value === "paused") return;
+
+        if (x instanceof Array) {
+          this.#status$.next("running");
+          for await (const result of TaskQueue.asyncLoopGenerator(
+            x,
+            execution
+          )) {
+            this.#tasks[result] = null;
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (this.#status$.value === "pause") break;
+          }
+
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          if (this.#status$.value === "pause") return;
+
+          this.#tasks = [];
+          this.#status$.next("idle");
+        }
+
+        console.groupEnd();
+      });
+
+    this.#taskSourceSubscription = this.#tasksConsumer$?.subscribe((x) => {
+      switch (this.#status$.value) {
+        case "idle":
+          if (this.#tasks.length < this.#taskPacakgeSize + 1) {
+            this.#tasks.push(x as Task);
+            this.#queue$.next(this.#tasks as Task[]);
+          } else {
+            this.#status$.next("running");
+            this.#taskPackages.push(of(x as Task));
+          }
+
+          break;
+
+        case "paused":
+        case "running":
+        default:
+          this.#taskPackages.push(of(x as Task));
+          break;
+      }
+    });
+
+    this.#statusSubscription = this.#status$.subscribe((status) => {
+      console.warn(`[Custom Events]: TaskQueue::${status}`);
+      if (status === "idle" && this.#taskPackages.length > 0) {
+        zip(this.#taskPackages.splice(0, this.#taskPacakgeSize))
+          .pipe(
+            mergeMap((i) => {
+              // Manipulate the tasks in TasksPackageQueue before sending them to the queue
+              // Example:
+              // 1. If the tasks have [priority] property, sort them by priority
+              // 2. If the tasks have [delay] property, delay them by delay
+              // 3. If the tasks have [interval] property, delay them by interval
+              // 4. If the tasks have [sequence] property, delay them by sequence
+              // 5. If the tasks have [parallel] property, delay them by parallel
+              return of(i);
+            }),
+            concatMap((x) => {
+              this.#queue$.next(x as Task[]);
+              return x;
+            })
+          )
+          .subscribe();
+      }
+    });
+
+    return this;
+  }
+}
